4f6746e28d590e0c58a2c2d8883aa15e9c50575d
kapil <kapilrajkc10@gmail.com>
2025-08-14 22:00:18 +0545

    Update loan.js

--- routes/loan.js
+++ routes/loan.js
@@ -3,11 +3,15 @@ const {
   createLoan,
   updateLoan,
   deleteLoan,
+  getLoans,
+  getLoanById
 } = require("../controllers/loanController");  
 const auth = require("../middleware/auth");
 
 const router = express.Router();
 
+router.get("/", auth(["admin", "user"]), getLoans);
+router.get("/:id", auth(["admin", "user"]), getLoanById);
 router.post("/", auth(["admin"]), createLoan);
 router.put("/:id", auth(["admin"]), updateLoan);
 router.delete("/:id", auth(["admin"]), deleteLoan);
