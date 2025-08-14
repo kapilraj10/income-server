5fc1d07e622674c375fd240e9353ad79242128f0
kapil <kapilrajkc10@gmail.com>
2025-08-14 18:34:10 +0545

    Update Income.js

--- routes/Income.js
+++ routes/Income.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { createIncome, getAll, updateIncome, deleteIncome } = require("../controllers/incomeController");
+const { createIncome,  updateIncome, deleteIncome } = require("../controllers/incomeController");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/", auth(["admin"]), createIncome);
-router.get("/", auth(), getAll);
 router.put("/:id", auth(["admin"]), updateIncome);
 router.delete("/:id", auth(["admin"]), deleteIncome);
 
